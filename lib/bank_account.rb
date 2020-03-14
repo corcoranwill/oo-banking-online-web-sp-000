d23a500d2e4a25366f5442be7c40ff37b76e9904
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 15:50:34 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
-  attr_accessor :balance, :status
   attr_reader :name
+  attr_accessor :balance, :status
 
   def initialize(name)
     @name = name
@@ -8,20 +8,20 @@ class BankAccount
     @status = "open"
   end
 
-  def deposit(deposit)
-    self.balance += deposit
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
   end
 
   def display_balance
-    "Your balance is $#{self.balance}."
-  end
-
-  def valid?
-    self.status == "open" && self.balance > 0
+    "Your balance is $#{balance}."
   end
 
   def close_account
     self.status = "closed"
+  end
+
+  def valid?
+    balance > 0 && status == "open"
   end
 
 end
