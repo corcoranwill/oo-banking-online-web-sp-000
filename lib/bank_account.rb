572a7c38566570eb2e5b4b66125ff113d48e4f78
Will Corcoran <corcoranwill@gmail.com>
2020-03-14 15:52:44 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,6 +2,7 @@ class BankAccount
   attr_reader :name
   attr_accessor :balance, :status
 
+  # initialize BankAccount w/ name, balance = 1000, status = "open"
   def initialize(name)
     @name = name
     @balance = 1000
